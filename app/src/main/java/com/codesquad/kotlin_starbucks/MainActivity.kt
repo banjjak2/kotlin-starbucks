337d62eb8d15d964b4dfcc9f54bfe9af9d2076e6
banjjak2 <interr0bang@naver.com>
2022-05-19 12:34:12 +0900

    feat: Bottom Navigation 세팅 및 그래프 설정

--- app/src/main/java/com/codesquad/kotlin_starbucks/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlin_starbucks/MainActivity.kt
@@ -2,10 +2,19 @@ package com.codesquad.kotlin_starbucks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setupWithNavController(navController)
     }
 }
